0161e724c23c8faf713c96ab33ba11e83ad0c5f7
ROMAN BADILLO GONZALEZ <rbadillo@elektra.com.mx>
2022-01-14 16:42:35 -0600

    Maquetado y colocación de elementos

--- src/app/shared/components/media-player/media-player.component.ts
+++ src/app/shared/components/media-player/media-player.component.ts
@@ -3,13 +3,16 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-media-player',
   templateUrl: './media-player.component.html',
-  styleUrls: ['./media-player.component.css']
+  styleUrls: ['./media-player.component.css'],
 })
 export class MediaPlayerComponent implements OnInit {
+  mockCover: any = {
+    cover: 'https://i.scdn.co/image/ab67616d0000b27345ca41b0d2352242c7c9d4bc',
+    album: 'Gioli & Assia',
+    name: 'BEBE (Oficial)',
+  };
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -7,6 +7,6 @@ import { HeaderUserComponent } from './components/header-user/header-user.compon
 @NgModule({
   declarations: [SideBarComponent, MediaPlayerComponent, HeaderUserComponent],
   imports: [CommonModule],
-  exports: [SideBarComponent],
+  exports: [SideBarComponent, MediaPlayerComponent, HeaderUserComponent],
 })
 export class SharedModule {}
