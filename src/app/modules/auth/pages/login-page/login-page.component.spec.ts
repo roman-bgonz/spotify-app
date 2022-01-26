53d9d3347000b6faca0f03bd341dc69e01b81be4
ROMAN BADILLO GONZALEZ <rbadillo@elektra.com.mx>
2022-01-26 15:37:21 -0600

    Pruebas iniciales de componente

--- src/app/modules/auth/pages/login-page/login-page.component.spec.ts
+++ src/app/modules/auth/pages/login-page/login-page.component.spec.ts
@@ -2,6 +2,7 @@ import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { LoginPageComponent } from './login-page.component';
 import { RouterTestingModule } from '@angular/router/testing';
+import { By } from '@angular/platform-browser';
 
 describe('LoginPageComponent', () => {
   let component: LoginPageComponent;
@@ -20,7 +21,46 @@ describe('LoginPageComponent', () => {
     fixture.detectChanges();
   });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
+  // TODO: Primer enunciado que debe asegurar los siguiente
+  // TODO: Debe de asegurarse que el formulario sea inválido cuando ingrese datos erroneos
+  it('😠🍎  Should return invalid form', () => {
+    // TODO: Patrón AAA (Arrange, act, assert)
+    // TODO Arrange (Declare values to use)
+    const mockCreds = {
+      email: '0x0x0x0',
+      password: '1234444242424424424424',
+    };
+    const emailForm = component.formLogin.get('email');
+    const passwordForm = component.formLogin.get('password');
+
+    // TODO: Act
+    emailForm?.setValue(mockCreds.email);
+    passwordForm?.setValue(mockCreds.password);
+
+    // TODO: Assert
+    expect(component.formLogin.invalid).toBeTrue();
+  });
+
+  it('🤖✈  Should return valid form', () => {
+    // TODO Arrange (Declare values to use)
+    const mockCreds = {
+      email: 'test@test.com',
+      password: '12345678',
+    };
+    const emailForm = component.formLogin.get('email');
+    const passwordForm = component.formLogin.get('password');
+
+    // TODO: Act
+    emailForm?.setValue(mockCreds.email);
+    passwordForm?.setValue(mockCreds.password);
+
+    // TODO: Assert
+    expect(component.formLogin.valid).toBeTrue();
+  });
+
+  it('🤝 The button should have in it the word Iniciar Sesión', () => {
+    const elRef = fixture.debugElement.query(By.css('.form-action button'));
+    const getInnerText = elRef.nativeElement.innerText;
+    expect(getInnerText).toEqual('Iniciar sesión');
   });
 });
